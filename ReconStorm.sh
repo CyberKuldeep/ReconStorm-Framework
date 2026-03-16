7a72e680ac2c566e41a5f06b3138e6c4b4aad77f
Kuldeep <kuldeepsmarttech@gmail.com>
2026-03-16 15:37:31 +0530

    Create ReconStorm.sh for automated domain scanning
    Added a Bash script for domain reconnaissance and scanning.

--- ReconStorm.sh
+++ ReconStorm.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+domain=$1
+
+if [ -z "$domain" ]; then
+ echo "Usage: ./ReconStorm.sh target.com"
+ exit
+fi
+
+source config/api_keys.conf
+
+date=$(date +%F)
+base_dir="output/${domain}-${date}"
+
+mkdir -p "$base_dir"/{recon,scan,enum,vuln,exploit,intelligence,report}
+
+echo "[+] Starting BugSploit Framework on $domain"
+
+bash modules/recon.sh $domain $base_dir
+bash modules/scanning.sh $domain $base_dir
+bash modules/enumeration.sh $domain $base_dir
+bash modules/vulnscan.sh $domain $base_dir
+bash modules/exploitation.sh $domain $base_dir
+bash modules/intelligence.sh $domain $base_dir
+bash modules/reporting.sh $domain $base_dir
+
+echo "[+] Scan Complete
