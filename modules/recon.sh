8d50550d99354890a10525de4c76604d06f24897
Kuldeep <kuldeepsmarttech@gmail.com>
2026-03-18 17:08:15 +0530

    Create recon.sh

---
+++ modules/recon.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+domain=$1
+base=$2
+
+echo "[+] Running Recon..."
+
+subfinder -d $domain -silent > $base/recon/subfinder.txt &
+assetfinder --subs-only $domain > $base/recon/assetfinder.txt &
+amass enum -passive -d $domain > $base/recon/amass.txt &
+
+wait
+
+cat $base/recon/*.txt | sort -u > $base/recon/subdomains.txt
+
+httpx -l $base/recon/subdomains.txt \
+ -threads 200 \
+ -status-code \
+ -title \
+ -tech-detect \
+ -web-server \
+ -ip \
+ -cdn \
+ -follow-redirects \
+ -silent \
+ -o $base/recon/live_hosts.txt
+
+gau $domain > $base/recon/urls.txt
+
+katana -list $base/recon/live_hosts.txt -silent > $base/recon/crawled_urls.txt
